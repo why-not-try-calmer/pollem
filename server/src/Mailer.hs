956852500be94befcbce72f75bdb009e94dd7df2
Nycticorax <mrnycticorax@gmail.com>
2021-03-14 23:45:20 +0100

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- server/src/Mailer.hs
+++ server/src/Mailer.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Mailer where
 
 import           Control.Monad.IO.Class
 import           Data.Aeson
+import           Data.Aeson.TH
 import qualified Data.Text              as T
 import           Network.HTTP.Req
 
@@ -11,37 +13,44 @@ data From = From {
     from_email :: T.Text,
     from_name  :: T.Text
 }
+$(deriveJSON defaultOptions ''From)
 
 data ReplyTo = ReplyTo {
     reply_email :: T.Text,
     reply_name  :: T.Text
 }
+$(deriveJSON defaultOptions ''ReplyTo)
 
 newtype Content = Content { content :: [T.Text ] }
+$(deriveJSON defaultOptions ''Content)
 
 data EmailData = EmailData {
     _from     :: From,
     _reply_to :: ReplyTo,
     _subject  :: T.Text
 }
+$(deriveJSON defaultOptions ''EmailData)
 
 newtype Personalization = Personalization { personalization :: [T.Text] }
+$(deriveJSON defaultOptions ''Personalization)
 
 newtype EmailBody = EmailBody {
     _personalizations :: [Personalization]
 }
+$(deriveJSON defaultOptions ''EmailBody)
 
 data Email = Email {
     email_url    :: T.Text,
     email_header :: T.Text,
     email_data   :: EmailData
 }
+$(deriveJSON defaultOptions ''Email)
 
 sendEmail :: IO ()
 sendEmail = runReq defaultHttpConfig $ do
-    let h = header "authorization: Bearer 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" "content-type: application/json"
+    let h = oAuth2Bearer "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"
         d = "{'personalizations':[{'to':[{'email':'adrien.glauser@gmail.com','name':'John Doe'}],'subject':'Hello, World!'}],'from':{'email':'mrnycticorax@gmail.com','name':'Géraud Lernais'},'reply_to':{'email':'mrnycticorax@gmail.com','name':'géraud Lernais'}}" :: T.Text
-    v <- req POST (https "https://api.sendgrid.com/v3/mail/send" /: "post") (ReqBodyJson d) jsonResponse mempty
+    v <- req POST (https "https://api.sendgrid.com/v3/mail/send" /: "post" ) (ReqBodyJson d) jsonResponse h
     liftIO $ print (responseBody v :: Value)
 
 main = sendEmail
