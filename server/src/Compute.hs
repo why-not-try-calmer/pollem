28cf7606f031bd10a48516d5a0254fbb2d61a483
mrnycticorax@gmail.com <mrnycticorax@gmail.com>
2021-03-19 20:24:07 +0100

    improved addAll
    Signed-off-by: mrnycticorax@gmail.com <mrnycticorax@gmail.com>

--- server/src/Compute.hs
+++ server/src/Compute.hs
@@ -5,31 +5,11 @@ import           Data.Foldable (foldl')
 import           Data.List     (transpose)
 import qualified Data.Text     as T
 
-addAll :: Num a => [[a]] -> Either T.Text [a]
+addAll :: (Num a, Eq a) => [[a]] -> Either T.Text [a]
 addAll [] = Left "Empty list. Borked data probably."
 addAll [x] = Right x
 addAll ls =
-    let lengths = map length ls
-        z = length . head $ ls
-        go = foldl' (zipWith (+)) (filler [] z) ls
-    in  if null go || minimum lengths /= maximum lengths
-        then Left "Empty result due to missing or corrupted data."
-        else Right go
-    where   filler l end = if length l < end then filler (0:l) end else l
-
-addAll' :: Num a => [[a]] -> Either T.Text [a]
-addAll' [] = Left "Empty list. Borked data probably."
-addAll' [x] = Right x
-addAll' ls =
-    let lengths = map length ls
+    let (x:xs) = ls
         go = map sum $ transpose ls
-    in  if minimum lengths /= maximum lengths then Left "Empty list. Borked data probably."
+    in  if any (/= x) xs then Left "Empty list. Borked data probably."
         else Right go
-
-main = do
-    let x = [[0,1],[0,0],[1,0],[1,1]]
-        y = [[0,1],[0,0],[0],[1,1]]
-    print $ addAll x
-    print $ addAll x
-    print $ addAll' y
-    print $ addAll' y
