20ef6d51dabfbd191b5b3060a1c23b99e394b352
Nycticorax <mrnycticorax@gmail.com>
2021-03-29 22:43:04 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- server/src/Database.hs
+++ server/src/Database.hs
@@ -19,54 +19,40 @@ import           Database.Redis
 import           ErrorsReplies
 import qualified ErrorsReplies          as ER
 
--- Data types
-{-
-class Submits a where
-    submit' :: a -> T.Text
+--
 
-instance Submits SubmitPoll where
-    submit' (SubmitPoll i r s a) = "submit poll"
+{-- Requests to db: types --}
 
-instance Submits SubmitClosePoll where
-    submit' (SubmitClosePoll i) = "submit close poll"
--}
+--
 
-data SubmitPoll = SubmitPoll {
-    create_poll_id        :: B.ByteString,
-    create_poll_recipe    ::  B.ByteString,
-    create_poll_startDate :: B.ByteString,
-    create_poll_active    :: B.ByteString
-}
+data DbReq =
+    SPoll {
+        create_poll_id        :: B.ByteString,
+        create_poll_recipe    ::  B.ByteString,
+        create_poll_startDate :: B.ByteString,
+        create_poll_active    :: B.ByteString
+        } |
+    SClose  { close_poll_id :: B.ByteString } |
+    SAsk {
+        ask_has         :: B.ByteString,
+        ask_fingerprint :: B.ByteString,
+        ask_token       :: B.ByteString
+    } |
+    SConfirm {
+        confirm_hash  :: B.ByteString,
+        confirm_token :: B.ByteString
+    } |
+    SAnswer {
+        answers_hash        :: B.ByteString,
+        answers_fingerprint :: B.ByteString,
+        answers_poll_id     :: B.ByteString,
+        answers_answers     :: B.ByteString
+    }
+--
 
-newtype SubmitClosePoll = SubmitClosePoll { close_poll_id :: B.ByteString }
+{-- Requests to db: functions --}
 
-data SubmitAskToken = SubmitAskToken {
-    ask_has         :: B.ByteString,
-    ask_fingerprint :: B.ByteString,
-    ask_token       :: B.ByteString
-}
-
-data SubmitConfirmToken = SubmitConfirmToken {
-    confirm_hash  :: B.ByteString,
-    confirm_token :: B.ByteString
-}
-
-data SubmitAnswers = SubmitAnswers {
-    answers_hash        :: B.ByteString,
-    answers_fingerprint :: B.ByteString,
-    answers_poll_id     :: B.ByteString,
-    answers_answers     :: B.ByteString
-}
-
-data Submit a where
-    SPoll :: SubmitPoll -> Submit a
-    SClosePoll :: SubmitClosePoll -> Submit a
-    SAskToken :: SubmitAskToken -> Submit a
-    SConfirmToken :: SubmitConfirmToken -> Submit a
-    SAnswerPoll :: SubmitAnswers -> Submit a
-
--- Requests to database
-
+--
 openConnection :: RedisConfig -> ConnectInfo
 openConnection conf = defaultConnectInfo {
     connectHost = host conf,
@@ -80,8 +66,8 @@ connDo config action = withConnect (openConnection config) (`runRedis` action)
 _connDo :: RedisConfig  -> Redis a -> IO ()
 _connDo config = void . connDo config
 
-submit :: Submit a -> Redis (Either (Err T.Text) (Ok T.Text))
-submit (SPoll (SubmitPoll pollid recipe startdate isactive)) =
+submit :: DbReq -> Redis (Either (Err T.Text) (Ok T.Text))
+submit (SPoll pollid recipe startdate isactive) =
     let pollid_txt = decodeUtf8 pollid
     in  exists ("poll:" `B.append` pollid) >>= \case
         Left err -> return . Left . ER.Err Database $ mempty
@@ -91,8 +77,8 @@ submit (SPoll (SubmitPoll pollid recipe startdate isactive)) =
                 hmset pollid [("recipe",recipe),("startDate",startdate),("active",isactive)]
                 return .  Right . ER.Ok $ "Added poll " `T.append` pollid_txt
 
-submit (SClosePoll (SubmitClosePoll pollid)) = return . Right . ER.Ok $ "ok"
-submit (SAskToken (SubmitAskToken hash fingerprint token)) =
+submit (SClose pollid) = return . Right . ER.Ok $ "ok"
+submit (SAsk hash fingerprint token) =
     let key = "user:" `B.append` hash
     in  exists key >>= \case
         Left err -> return . Left . ER.Err UserNotExist $ T.pack . show $ err
@@ -101,7 +87,7 @@ submit (SAskToken (SubmitAskToken hash fingerprint token)) =
             else do
                 hmset key [("fingerprint", fingerprint),("token", token),("verified", "false")]
                 return . Right . ER.Ok $ "Thanks, please check your email"
-submit (SConfirmToken (SubmitConfirmToken hash token)) =
+submit (SConfirm hash token) =
     let key = "user:" `B.append` hash
     in  exists key >>= \case
         Left err -> return . Left . ER.Err Database $ T.pack . show $ err
@@ -116,7 +102,34 @@ submit (SConfirmToken (SubmitConfirmToken hash token)) =
                             hmset key [("verified","true")]
                             return . Right . ER.Ok $ "Thanks, you've successfully confirmed your email address."
                         else return . Left . ER.Err UserNotExist $ decodeUtf8 hash
-submit (SAnswerPoll (SubmitAnswers hash finger pollid answers)) =
+submit (SAnswer hash finger pollid answers) =
+    let pollid_txt = decodeUtf8 pollid
+    in  hgetall ("poll:" `B.append` pollid) >>= \case
+            Left err -> return . Left .ER.Err PollNotExist $ pollid_txt
+            Right keys_values -> do
+                let userKey = "user:" `B.append` hash
+                if not $ meetConditions [("active","true")] keys_values then return . Left . ER.Err PollInactive $ pollid_txt
+                else hgetall userKey >>= \case
+                    Left err -> return . Left . ER.Err UserNotExist $ decodeUtf8 hash
+                    Right user_keys ->
+                        if not $ meetConditions [("verified", "true")] user_keys then return . Left . ER.Err UserUnverified $ decodeUtf8 hash
+                        else sismember hash ("participants_hashes:" `B.append` pollid) >>= \case
+                            Left err -> return . Left . ER.Err Database $ mempty
+                            Right verdict ->
+                                if not verdict then return . Left . ER.Err PollTakenAlready $ pollid_txt
+                                else sismember hash ("participants_fingerprints:" `B.append` finger) >>= \case
+                                    Left err -> return . Left . ER.Err Database $ mempty
+                                    Right verdict ->
+                                        if not verdict then return . Left . ER.Err PollTakenAlready $ pollid_txt
+                                        else multiExec ( do
+                                            sadd ("participants_hashes:" `B.append` pollid) [hash]
+                                            sadd ("participants_fingerprints" `B.append` pollid) [finger]
+                                            set ("answers:" `B.append` pollid `B.append` hash) answers
+                                        ) >>= \case TxSuccess _ -> return . Right . ER.Ok $ "Answers submitted successfully!"
+                                                    _  -> return . Left . ER.Err Database $ "Database error"
+    where   meetConditions keyvals = all (`elem` keyvals)
+
+submit' (SAnswer hash finger pollid answers) =
     let pollid_txt = decodeUtf8 pollid
     in  hgetall ("poll:" `B.append` pollid) >>= \case
             Left err -> return . Left .ER.Err PollNotExist $ pollid_txt
