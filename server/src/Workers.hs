8e9872fb2def91b32b9bab38d2dc3d384509cb51
Nycticorax <mrnycticorax@gmail.com>
2021-04-01 19:16:31 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- server/src/Workers.hs
+++ server/src/Workers.hs
@@ -13,24 +13,30 @@ import qualified ErrorsReplies            as R
 import           Scheduler                (getNow, isoOrCustom)
 
 runSweeper :: Connection -> IO ()
-runSweeper conn =
-    let startWorker =
-            withAsync (forever $ do
-                print "Sweeping once and then sleeping for one hour..."
-                now <- getNow
-                res <- connDo conn $ sweeper >>= \case
-                    Right pollidDate ->
-                        let filtered (i, d) acc = case isoOrCustom . show $ d of
-                                Left notParsed -> acc
-                                Right valid_date -> if valid_date > now then i : acc else acc
-                            outdated = foldr filtered [] pollidDate
-                        in  disablePolls outdated
-                case res of
-                    Left err  -> print . R.renderError $ err
-                    Right msg -> print . R.renderOk $ msg
-                threadDelay $ 10000000 * 3600
-            ) wait
-    in  startWorker `catch` \e -> let e' = e :: SomeException in startWorker
+runSweeper conn = 
+    let toDo = withAsync ( do
+            print "Sweeping once and then sleeping for one hour..."
+            now <- getNow
+            res <- connDo conn $ sweeper >>= \case
+                Right pollidDate ->
+                    let filtered (i, d) acc = case isoOrCustom . show $ d of
+                            Left notParsed -> acc
+                            Right valid_date -> if valid_date > now then i : acc else acc
+                        outdated = foldr filtered [] pollidDate
+                    in  disablePolls outdated
+            case res of
+                Left err  -> print . R.renderError $ err
+                Right msg -> print . R.renderOk $ msg
+            threadDelay $ 1000000 * 3600
+            )
+        while th = waitCatch th >>= \case
+            Left e ->
+                let e' = e :: SomeException
+                in  do
+                    print . show $ e
+                    runSweeper conn
+            Right _ -> runSweeper conn
+    in  toDo while
 
 main :: IO ()
 main = initRedisConnection >>= runSweeper
