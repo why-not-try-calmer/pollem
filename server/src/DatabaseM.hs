bdbc5e7b67aae64578b0769ecf890f1c4e2f7a1b
Nycticorax <mrnycticorax@gmail.com>
2021-05-12 13:39:46 +0200

    ---
    Signed-off-by: Nycticorax <mrnycticorax@gmail.com>

--- server/src/DatabaseM.hs
+++ server/src/DatabaseM.hs
@@ -10,20 +10,82 @@ import qualified Data.Text                      as T
 import           Database.MongoDB
 import qualified Database.MongoDB.Transport.Tls as DbTLS
 import           HandlersDataTypes
+--
 
+{- Types -}
+
+--
 data MongoCreds = MongoCreds {
     shard :: String,
     user  :: T.Text,
     pwd   ::T.Text
 } deriving (Eq, Show)
 
+data DbReq =
+    SCreate {
+        create_poll_hash      :: T.Text,
+        create_poll_email     :: T.Text,
+        create_poll_token     :: T.Text,
+        create_poll_id        :: T.Text,
+        create_poll_recipe    :: T.Text,
+        create_poll_startDate :: T.Text,
+        create_poll_endDate   :: Maybe T.Text,
+        create_poll_secret    :: T.Text
+    } |
+    SClose  {
+        close_hash    :: T.Text,
+        close_token   :: T.Text,
+        close_poll_id :: T.Text
+    } |
+    SAsk {
+        ask_hash  :: T.Text,
+        ask_token :: T.Text
+    } |
+    SConfirm {
+        confirm_token       :: T.Text,
+        confirm_fingerprint :: T.Text,
+        confirm_hash        :: T.Text
+    } |
+    STake {
+        answers_hash        :: T.Text,
+        answers_token       :: T.Text,
+        answers_fingerprint :: T.Text,
+        answers_email       :: T.Text,
+        answers_poll_id     :: T.Text,
+        answers_answers     :: [T.Text]
+    } |
+    SGet { get_poll_id :: T.Text }
+
+data BSONable = BSPoll { _poll :: Poll } | BSReq { _req :: DbReq }
+
+toBSON :: BSONable -> [Field]
+toBSON (BSPoll (Poll s e q d m v a)) =
+    let def = ["startDate" =: val s, "question" =: val q, "description" =: val d, "multiple" =: val m, "visible" =: val v, "answers" =: val a]
+    in  case e of
+        Nothing      -> def
+        Just endDate -> def ++ ["endDate" =: val endDate]
+toBSON (BSReq (SCreate h em tok i recipe start end sec)) =
+    let def = ["hash" =: val h, "email" =: val em, "token" =: val tok, "recipe" =: val recipe, "startDate" =: val start, "secret" =: sec]
+    in  case end of
+        Nothing      -> def
+        Just endDate -> def ++ ["endDate" =: val endDate]
+toBSON (BSReq (SClose h t pid)) = ["hash" =: h, "token" =: t, "poll_id" =: pid]
+toBSON (BSReq (SAsk h t)) = ["hash" =: h, "token" =: t]
+toBSON (BSReq (SConfirm h fi t)) = ["hash" =: h, "fingerprint" =: fi, "token" =: t]
+toBSON (BSReq (STake h t fi em pid ans)) = ["hash" =: h, "token" =: t, "fingerprint" =: fi, "email" =: em, "poll_id" =: pid, "answers" =: ans]
+--
+
+{- Auth, Connection -}
+
+--
 initMongCreds :: MongoCreds
 initMongCreds = MongoCreds "cluster0-shard-00-02.cmocx.mongodb.net" "pollem-app" "FmUCY0OkZVHJ1MUY"
 
--- FIX ME: MongoAtlas tends to shuffle around the role of 'primary' versus 'secondary' shard
--- Make sure to call selectOK to avoid failing to authenticate
-
 testAccess :: IO ()
+{-
+    FIX ME: MongoAtlas tends to shuffle around the role of 'primary' versus 'secondary' shard
+    Make sure to call selectOK to avoid failing to authenticate
+-}
 testAccess =
     let creds = initMongCreds
     in  try (DbTLS.connect (shard creds) (PortNumber 27017)) >>= \case
@@ -34,29 +96,50 @@ testAccess =
             close pipe
     where
         runIt = do
-            insert "polls" . toBSON . BSPoll $ makeAPoll
-            findOne (select ["name" =: "Samuel"] "polls") >>= liftIO . print
+            --createPoll ["_id" =: "007"]
+            insertParticipation "007" "myhash" "lelaStar" "finger" [0,1,0]
+            insertParticipation "007" "myhash" "vanessLane" "finger" [1,1,0]
+            getPollParticipations "007" >>= rest >>= liftIO . print
+--
 
-data Meta = Meta Int Int Int Int Int (Maybe Int)
+{- Users -}
 
+--
+checkIfExistsUser hash = findOne (select ["_id" =: hash] "users") >>= \case
+    Just user -> pure True
+    Nothing   -> pure False
+getIfExistsUser hash = findOne (select ["_id" =: hash] "users")
+getIfUserWithToken hash token = findOne (select ["_id" =: hash, "token" =: token] "users")
+getIfUserWithFields hash otherfields = findOne (select (("_id" =: hash) : otherfields) "users")
+updateUserWith hash otherfields = upsert (select (("_id" =: hash) : otherfields) "users")
+createUser hash fingerprint = insert "users" ["_id" =: hash, "fingerprint" =: fingerprint, "verified" =: "true"]
+--
+
+{- Polls -}
+
+--
+getIfExistsPoll poll_id = findOne (select ["_id" =: poll_id] "polls")
+getIfPollWithFields poll_id otherfields = findOne (select (("_id" =: poll_id) : otherfields) "polls")
+createPoll poll_id otherfields = insert "polls" (("_id" =: poll_id) : otherfields)
+updatePollWith poll_id otherfields = upsert (select (("_id" =: poll_id) : otherfields) "polls")
+--
+
+{- Participations -}
+
+--
+insertParticipation poll_id hash email fingerprint answers = insert ("polls." `T.append` poll_id) ["poll_id" =: poll_id, "email" =: email, "fingerprint" =: fingerprint, "answers"=: answers]
+getPollParticipations poll_id = find (select [] ("polls." `T.append` poll_id))
+--
+
+{- Operations -}
+
+--
+main :: IO ()
+main = testAccess
+--
+
+{- Tests -}
+
+--
 makeAPoll :: Poll
 makeAPoll = Poll "start_date" (Just "end_date") "question" "description" True True ["Answer A", "Answer B"]
-
-data BSONable = BSPoll { _poll :: Poll } | BSMeta { _meta :: Meta }
-
-toBSON :: BSONable -> [Field]
-toBSON (BSPoll (Poll s e q d m v a)) =
-    let def = ["startDate" =: val s, "question" =: val q, "description" =: val d, "multiple" =: val m, "visible" =: val v, "answers" =: val a]
-    in  case e of
-        Nothing      -> def
-        Just endDate -> def ++ ["endDate" =: val endDate]
-toBSON (BSMeta (Meta h em t r s en)) =
-    let def = ["hash" =: val h, "email" =: val em, "token" =: val t, "recipe" =: val r, "startDate" =: val s]
-    in  case en of
-        Nothing     -> def
-        Just enDate -> def ++ ["endDate" =: val en]
-
-main :: IO ()
-main = do
-    let a = BSPoll makeAPoll
-    print $ toBSON a
